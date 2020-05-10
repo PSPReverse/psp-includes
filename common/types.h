c42f0b7631c6d1fa7c17695aae1cdd938482b6b5
Alexander Eichner <alexander.eichner@campus.tu-berlin.de>
2020-05-10 12:47:42 +0200

    Address space enumeration for use in multiple sub projects

--- common/types.h
+++ common/types.h
@@ -1,5 +1,30 @@
-#ifndef __include_types_h
-#define __include_types_h
+/** @file
+ * PSP common - Type definitions shared between projects.
+ */
+
+/*
+ * Copyright (C) 2020 Alexander Eichner <alexander.eichner@campus.tu-berlin.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#ifndef INCLUDED_common_types_h
+#define INCLUDED_common_types_h
 
 #ifdef IN_PSP
 typedef unsigned char      uint8_t;
@@ -47,4 +72,30 @@ typedef uint64_t X86PADDR;
 /** R0 pointer. */
 typedef uint64_t R0PTR;
 
-#endif
+
+/**
+ * PSP address space type.
+ */
+typedef enum PSPADDRSPACE
+{
+    /** Invalid address space. */
+    PSPADDRSPACE_INVALID = 0,
+    /** Generic PSP address. */
+    PSPADDRSPACE_PSP,
+    /** PSP SRAM. */
+    PSPADDRSPACE_PSP_MEM,
+    /** PSP MMIO. */
+    PSPADDRSPACE_PSP_MMIO,
+    /** SMN. */
+    PSPADDRSPACE_SMN,
+    /** Generic x86 address. */
+    PSPADDRSPACE_X86,
+    /** x86 standard memory. */
+    PSPADDRSPACE_X86_MEM,
+    /** x86 MMIO. */
+    PSPADDRSPACE_X86_MMIO,
+    /** 32bit hack. */
+    PSPADDRSPACE_32BIT_HACK = 0x7fffffff
+} PSPADDRSPACE;
+
+#endif /* !INCLUDED_common_types_h*/
